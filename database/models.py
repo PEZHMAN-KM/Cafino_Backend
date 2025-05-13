88f48fce5a28666655b719e9745066bfe77d54d8
Hossein <hossienbehzadi1381@gmail.com>
2025-05-13 15:24:36 +0330

    database tables added

--- database/models.py
+++ database/models.py
@@ -18,3 +18,56 @@ class User(ID, Base):
     is_admin = Column(Boolean, default=False)
     is_super_admin = Column(Boolean, default=False)
     is_waitress = Column(Boolean, default=False)
+
+
+class Notification(ID, Base):
+    table_number = Column(Integer, nullable=False)
+    add_time = Column(DateTime, nullable=False)
+    message = Column(String, nullable=True)
+    is_in_progress = Column(Boolean, default=False)
+    start_progress_time = Column(DateTime, nullable=True)
+    is_done = Column(Boolean, default=False)
+    done_time = Column(DateTime, nullable=True)
+    waitress_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    waitress_name = Column(String(150), nullable=True)
+
+
+
+class Category(ID, Base):
+    __tablename__ = 'category'
+    name = Column(String(150), nullable=False, unique=True)
+    pic_url = Column(String, nullable=True)
+
+
+
+class Food(ID, Base):
+    __tablename__ = 'food'
+    name = Column(String(150), nullable=False)
+    price = Column(Integer, nullable=False)
+    description = Column(String, nullable=True)
+    in_sale = Column(Boolean, default=False)
+    sale_price = Column(Integer, nullable=True)
+    category_id = Column(Integer, ForeignKey('category.id'), nullable=False)
+    pic_url = Column(String, nullable=True)
+
+
+class Order(ID, Base):
+    __tablename__ = 'order'
+    table_number = Column(Integer, nullable=False)
+    message = Column(String, nullable=True)
+    time_added = Column(DateTime, nullable=False)
+    is_accepted = Column(Integer, default=False)
+    accepted_time = Column(DateTime, nullable=True)
+    is_done = Column(Boolean, default=False)
+    done_time = Column(DateTime, nullable=True)
+    total_price = Column(Integer, nullable=True)
+    admin_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    admin_name = Column(String(150), nullable=True)
+
+
+class OrderFood(ID, Base):
+    __tablename__ = 'order_food'
+    order_id = Column(Integer, ForeignKey('order.id'), nullable=False)
+    food_id = Column(Integer, ForeignKey('food.id'), nullable=False)
+    food_price = Column(Integer, nullable=True)
+    quantity = Column(Integer, default=1)
