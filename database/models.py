29414eeabc247d9cf8cfbbd51ed1b1655fabf521
Hossein <hossienbehzadi1381@gmail.com>
2025-05-13 12:09:51 +0330

    add user model

---
+++ database/models.py
@@ -0,0 +1,20 @@
+from database.database import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean, Float, JSON
+
+
+# ID Class ==============================================================================================
+class ID:
+    __abstract__ = True
+    id = Column(Integer, unique=True, index=True, primary_key=True)
+
+
+# User Class ================================================================================================
+class User(ID, Base):
+    __tablename__ = "user"
+    username = Column(String(150), unique=True, nullable=False)
+    password = Column(String(150), nullable=False)
+    phone_number = Column(String(50), unique=True, nullable=True)
+    is_admin = Column(Boolean, default=False)
+    is_super_admin = Column(Boolean, default=False)
+    is_waitress = Column(Boolean, default=False)
+    is_banned = Column(Boolean, default=False)
