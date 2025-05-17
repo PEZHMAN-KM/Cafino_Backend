e7b93d2ca969d244e048bd57ca6b6986bb968dad
Hossein <hossienbehzadi1381@gmail.com>
2025-05-17 15:35:38 +0330

    notification functions

--- functions/notification_functions.py
+++ functions/notification_functions.py
@@ -1,38 +1,104 @@
+import datetime
+
 from database.models import Notification
 from sqlalchemy.orm import Session
 from sqlalchemy import delete, and_
 from errors.notification_errors import NOTIFICATION_NOT_FOUND_ERROR, NO_NOTIFICATION_FOUND_ERROR
-from schemas.notification_schemas import AddNotificationModel, UnprogressNotificationModel
+from schemas.notification_schemas import AddNotificationModel, GetNotifInProgress
 
 
 
 async def add_notif(notif: AddNotificationModel, db: Session):
-    ...
+    new_notif = Notification(
+        table_number=notif.table_number,
+        add_time=datetime.datetime.now(),
+        message=notif.message if notif.message else None
+    )
+
+    db.add(new_notif)
+    db.commit()
+    db.refresh(new_notif)
+
+    return new_notif
 
 
-async def get_notif_in_progress(waitress_id: int, db: Session):
-    ...
+async def get_notif_in_progress(request: GetNotifInProgress, db: Session):
+    notif = db.query(Notification).filter(Notification.id == request.notif_id).first()
+
+    if not notif:
+        raise NOTIFICATION_NOT_FOUND_ERROR
+
+    notif.in_progress = True
+    notif.waitress_id = request.waitress_id
+    notif.start_progress_time = datetime.datetime.now()
+    db.commit()
 
 
-async def get_out_of_progress(request: UnprogressNotificationModel, db: Session):
-    ...
+    return notif
 
 
-async def get_notif_done(request: UnprogressNotificationModel, db: Session):
-    ...
+
+async def get_out_of_progress(notif_id: int, db: Session):
+    notif = db.query(Notification).filter(Notification.id == notif_id).first()
+
+    if not notif:
+        raise NOTIFICATION_NOT_FOUND_ERROR
+
+    notif.in_progress = False
+    notif.waitress_id = None
+    notif.start_progress_time = None
+    db.commit()
+
+    return notif
 
 
-async def get_all_not_done_notifs(db: Session):
-    ...
+async def get_notif_done(notif_id: int, db: Session):
+    notif = db.query(Notification).filter(Notification.id == notif_id).first()
+
+    if not notif:
+        raise NOTIFICATION_NOT_FOUND_ERROR
+
+    notif.is_done = True
+    notif.done_time = datetime.datetime.now()
+    db.commit()
+
+    return notif
 
 
-async def get_all_in_progrees_notifs(db: Session):
-    ...
+async def get_all_requested_notifs(db: Session):
+    notifs = db.query(Notification).filter(and_(Notification.is_done == False, Notification.is_in_progress == False)).all()
+
+    return notifs
+
+
+async def get_all_in_progress_notifs(db: Session):
+    notifs = db.query(Notification).filter(and_(Notification.is_done == False, Notification.is_in_progress == True)).all()
+
+    return notifs
 
 
 async def get_notifs_to_show(db: Session):
-    ...
+    in_progress_notifs = get_all_in_progress_notifs(db)
+    requested_notifs = get_all_requested_notifs(db)
+
+    if not in_progress_notifs and not requested_notifs:
+        raise NO_NOTIFICATION_FOUND_ERROR
+
+    notifs_display = {
+        'in_progress_notifs': in_progress_notifs,
+        'requested_notifs': requested_notifs
+    }
+
+
+    return notifs_display
+
+
 
 
 async def get_notif(notif_id: int, db: Session):
-    ...
+    notif = db.query(Notification).filter(Notification.id == notif_id).first()
+
+    if not notif:
+        raise NOTIFICATION_NOT_FOUND_ERROR
+
+    return notif
--- schemas/notification_schemas.py
+++ schemas/notification_schemas.py
@@ -1,10 +1,27 @@
+import datetime
 from schemas.base_schemas import BaseSchema
 
 
 
 class AddNotificationModel(BaseSchema):
-    ...
+    table_number: int
+    message: str | None = None
 
 
-class UnprogressNotificationModel(BaseSchema):
-    ...
+class GetNotifInProgress(BaseSchema):
+    waitress_id: int
+    notif_id: int
+
+class NotificationDisplay(AddNotificationModel):
+    add_time: datetime.datetime
+    is_in_progress: bool
+    start_progress_time: datetime.datetime | None = None
+    is_done: bool
+    done_time: datetime.datetime | None = None
+    waitress_id: int | None = None
+    waitress_name: str | None = None
+
+
+class ShowNotifications(BaseSchema):
+    in_progress_notifs: list[NotificationDisplay]
+    requested_notifs: list[NotificationDisplay]
