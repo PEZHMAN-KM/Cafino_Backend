5969c5963bfa3a58993c7455e71c1663d628b3a8
Revenge KM <Kazemimir.pezhman@gmail.com>
2025-06-03 18:37:02 +0330

    add some data to data base

--- errors/food_errors.py
+++ errors/food_errors.py
@@ -6,5 +6,5 @@ from fastapi import status
 FOOD_NOT_FOUND_ERROR = HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                                      detail='Food Not Found')
 
-NO_FOOD_FOUND_ERROR = HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+NO_FOOD_FOUND_ERROR = HTTPException(status_code=status.HTTP_204_NO_CONTENT,
                                          detail='No Food Found')
